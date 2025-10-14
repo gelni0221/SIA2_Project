16d9ae9549c3cb18a7934966aa91130fb349df5e
gelni0221 <antipolonigel@gmail.com>
2025-10-14 13:41:24 +0800

    video_games ewan

---
+++ For testing/Steam API Testing/steamapi_test.py
@@ -0,0 +1,50 @@
+import requests
+import tkinter as tk
+import re
+
+root = tk.Tk()
+root.configure()
+
+root.geometry("1600x900")
+url = "https://store.steampowered.com/api/appdetails?appids=1687950"
+response = requests.get(url)
+# "header_image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1687950/header.jpg"
+# "capsule_image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1687950/capsule_184x69.jpg"
+# "background": "https://cdn.cloudflare.steamstatic.com/steam/apps/1687950/page_bg_generated.jpg"
+
+def clean_html(raw_html):
+    # Remove video/image tags entirely
+    cleaned = re.sub(r'<video.*?</video>', ' ', raw_html, flags=re.DOTALL)
+    # Remove all other HTML tags
+    cleaned = re.sub(r'<.*?>', ' ', cleaned)
+    # Remove URLs
+    cleaned = re.sub(r'http\S+', ' ', cleaned)
+    return cleaned.strip()
+
+
+if response.status_code == 200:
+    data = response.json()
+
+    app_data = data["1687950"]["data"]
+    print(app_data["name"])
+    print(app_data["release_date"]["date"])
+
+    print(app_data["developers"])
+    print(app_data["publishers"])
+    print(app_data["price_overview"]["final_formatted"])
+    print(app_data["short_description"])
+    minimum_specs = clean_html(app_data["pc_requirements"]["minimum"])
+    recc_specs = clean_html(app_data["pc_requirements"]["recommended"])
+    print(minimum_specs)
+    print(recc_specs)
+    print(app_data["website"])
+    for a in app_data["genres"]:
+        print(a["description"])
+    clean_data_game = clean_html(app_data["about_the_game"])
+    print(clean_data_game)
+
+    print(data)
+
+
+
+
--- video_games_function.py
+++ video_games_function.py
@@ -1,11 +1,46 @@
-import os
-#pip install python-dotenv
+from PIL import Image, ImageTk
+import tkinter as tk
+import requests
+from io import BytesIO
+import re
 
-from dotenv import load_dotenv
+class VideoGames:
 
-# FOR LOADING .ENV FILE FOR THE STEAM API KEY
-load_dotenv()
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.geometry("1600x900")
+        self.root.configure(bg="#1A1A1A")
+        self.header_image = "https://images.steamusercontent.com/ugc/2019355369832417157/0589A1EE3E6E97247D21B72A5DBB8E25ECAC171F/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false"
+        self.capsule_image = "https://cdn.cloudflare.steamstatic.com/steam/apps/1687950/capsule_184x69.jpg"
+        self.background = "https://store.akamai.steamstatic.com/images/storepagebackground/app/1687950?t=1733297467"
 
-# TO ACCESS THE API KEY
-steam_api_key = os.getenv("STEAM_API_KEY")
+        # self.bg_response = requests.get(self.background)
+        # self.bg_img = Image.open(BytesIO(self.bg_response.content))
+        # self.bg_img = self.bg_img.resize((1920,1080), resample=Image.LANCZOS)
+        # self.tk_bg_img = ImageTk.PhotoImage(self.bg_img)
+        # self.bg_label = tk.Label(self.root, image= self.tk_bg_img, bd=0)
+        # self.bg_label.place(x=0, y=0, relwidth=1, relheight=1)
 
+        self.hd_response = requests.get(self.header_image)
+        self.hd_img = Image.open(BytesIO(self.hd_response.content))
+        self.hd_img = self.hd_img.resize((616,353), resample=Image.LANCZOS)
+        self.tk_hd_img = ImageTk.PhotoImage(self.hd_img)
+        self.hd_label = tk.Label(self.root, image=self.tk_hd_img, bd=0)
+        self.hd_label.place(relx=1.0, y=50, anchor='ne', x=-40)
+        self.title_label = tk.Label(self.root, bd=0, text="Persona 5 Royal",font=('Arial', 30), bg="#1A1A1A", fg="white" )
+        self.title_label.place(x=40, y=50)
+
+        self.root.mainloop()
+
+    def remove(self, raw_html):
+        #removes video tags in the steam api
+        cleaned = re.sub(r'<video.*?</video>', ' ', raw_html, flags=re.DOTALL)
+        #this one the tags anything with <>
+        cleaned = re.sub(r'<.*?>', ' ', cleaned)
+        #this one with links
+        cleaned = re.sub(r'http\S+', ' ', cleaned)
+        return cleaned.strip()
+
+
+
+test1 = VideoGames()
